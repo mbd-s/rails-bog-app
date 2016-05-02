6d6c827eab6bb1f8023af032ae9088fad94693e8
mbd-s <michaelbsilverberg@gmail.com>
2016-05-02 00:08:32 -0700

    updating again

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -23,4 +23,18 @@ class CreaturesController < ApplicationController
     @creature = Creature.find_by_id(creature_id)
     render :show
   end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :edit
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creatures_path(creature)
+  end
 end
